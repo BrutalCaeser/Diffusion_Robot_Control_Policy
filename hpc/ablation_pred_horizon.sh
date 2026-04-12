d4b48d79c44aa394ba48c66cb5d2c051e0e24bfe
vineeth-sakhamuru <sakhamuruvineeth@gmail.com>
2026-04-12 11:49:19 -0700

    Fix env activation in ablation script

--- hpc/ablation_pred_horizon.sh
+++ hpc/ablation_pred_horizon.sh
@@ -29,10 +29,11 @@
 
 set -euo pipefail
 
+module load anaconda3/2024.06
 module load cuda/12.1.1
+source activate /home/$USER/envs/diffusion
 
 PROJECT=/scratch/$USER/diffusion_policy
-source $PROJECT/venv/bin/activate   # use project venv instead of conda
 DATASET=$PROJECT/data/pusht_cchi_v7_replay.zarr
 
 cd $PROJECT
